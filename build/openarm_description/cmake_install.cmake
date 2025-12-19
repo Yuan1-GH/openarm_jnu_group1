60f1fc443baaf650614ee8c83abf8445cb3bad23
yuan <1103887657@qq.com>
2025-12-19 21:00:14 +0800

    moveIt Demo接入

--- build/openarm_description/cmake_install.cmake
+++ build/openarm_description/cmake_install.cmake
@@ -111,10 +111,6 @@ if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/openarm_description" TYPE FILE FILES "/home/yuan/arm_ws/src/openarm_description/package.xml")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/openarm_description" TYPE DIRECTORY FILES "/home/yuan/arm_ws/src/openarm_description/meshes")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
